b23e0d9aa94810f380c3dfb3bac355958bf105c2
Micaela Micheltorena <micaela.micheltorena@gmail.com>
2022-04-10 09:57:25 -0300

    Solucionado el tema de las imagenes

--- index/views.py
+++ index/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def inicio(request):
   return render(request, "indice/index.html", {})
 
 def about(request):
   return render(request, "indice/about.html", {})
-
-
